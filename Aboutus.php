ac1546c2f8bd9b53c92e827f6278705b11c3ef4f
shubham <shubhamjangid3045@gmail.com>
2020-10-11 22:45:27 +0530

    Edited

--- Aboutus.php
+++ Aboutus.php
@@ -1,1 +1,13 @@
-Aboutus.html
+<!DOCTYPE html>
+<html>
+<head>
+	<title>About</title>
+</head>
+<body>
+	<div class="container-fluid">
+		<div class="row">
+			<h1>This is About....</h1>
+		</div>
+	</div>
+</body>
+</html>
